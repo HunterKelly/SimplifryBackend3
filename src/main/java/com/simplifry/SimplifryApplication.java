da63f6a3594d46f289e96af4f3961f7b29e90888
HunterKelly <Hunterkelly1234@gmail.comgit pullgit config --global user.email Hunterkelly1234@gmail.com>
2023-03-06 15:21:24 -0500

    initial commit

---
+++ src/main/java/com/simplifry/SimplifryApplication.java
@@ -0,0 +1,13 @@
+package com.simplifry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplifryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplifryApplication.class, args);
+	}
+
+}
